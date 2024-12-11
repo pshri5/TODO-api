2938bbd67e3fe3f76d1c3a274885665d894c14df
Preetam Shri <preetamsshri@gmail.com>
2024-12-11 09:42:49 +0530

    connect database

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 import express from "express"
-
+import cors from "cors"
 const app =express()
-const port= 3000
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+app.use(express.json({limit:"16kb" }))
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+
+
 
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,22 @@
-import express from "express";
-import cors from "cors"
 import dotenv from "dotenv"
+import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+
+const port = process.env.PORT || 8000
+
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection error",err)
+})
 
 
