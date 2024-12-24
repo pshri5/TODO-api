c0be8b18a4da27119a6ab53847c8a89f1bcd7fc1
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2024-12-24 19:13:54 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import taskRouter from "./routes/task.routes"
 
 const app =express()
 
@@ -13,7 +14,7 @@ app.use(
 app.use(cookieParser())
 app.use(express.json({limit:"16kb" }))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
-
+app.use("/api/v1/tasks", taskRouter)
 
 
 
